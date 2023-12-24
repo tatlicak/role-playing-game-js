ad74f36e7263f5efe0ae5e9f43c4e6af9d6391e4
Abdullah TATLICAK <tatlicak@gmail.com>
2023-12-24 17:20:09 +0300

    Step: 34-35-36

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ let fighting;
 let monsterHealth;
 let inventory = ['stick'];
 
-let button1 = document.querySelector("#button1");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+
+const monsterStats = document.querySelector("#monsterStat");
+const monsterName = document.querySelector("#monsterName");
+
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+// initialize buttons
