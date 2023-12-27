ff72fb31ed8e776db2b225e0c94ad441fba7b037
tatlicak <tatlicak@gmail.com>
2023-12-27 04:26:13 +0000

    Step: 57-58-59

--- script.js
+++ script.js
@@ -20,14 +20,37 @@ const monsterName = document.querySelector("#monsterName");
 
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-const locations = [{
+const locations = [
+    {
     name:"town square",
     "button text":[
         "Go to store", 
         "Go to cave", 
         "Fight dragon"
     ],
-}];
+    "button functions":[
+        goStore,
+        goCave,
+        fightDragon
+    ],
+    text:"You are in the town square. You see a sign that says \"Store\".",
+},
+{
+    name:"store",
+    "button text":[
+        "Buy 10 health (10 gold)",
+        "Buy weapon (30 gold)",
+        "Go to town square"
+    ],
+    "button functions":[
+        buyHealth,
+        buyWeapon,
+        goTown
+    ],
+    text:"You enter the store.",
+    
+}
+];
 
 // initialize buttons
 
